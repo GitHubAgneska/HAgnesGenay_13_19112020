fea7e2f0f0763a9c89f08505c99d338462c2559c
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2021-09-24 18:42:44 +0200

    app architecture base

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Home from ''
+import {Redirect} from 'react-router-dom/cjs/react-router-dom.min';
+import NavSide from './components/layout/Nav_side';
+import { GlobalStyle } from './style/global_style';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    
+    return (
+        <div className="App">
+            <div className="container" style={{height:'100%', width:'100%', position:'fixed'}}>
+                <GlobalStyle />
+                    <Router>
+                        <Header /> {/* INSIDE router because contains NAV with 'LINK TO'  */}
+                        <NavSide /> {/* same  */}
+                        <Fragment>
+                            <Switch>
+                                <Route exact path="/" render={() => <Redirect to="/home" />} />
+                                <Route exact path="/userProfile/:id" component={Home} />
+                            </Switch>
+                        </Fragment>
+                    </Router>
+            </div>
+        </div>
+    )
 }
+export default App
 
-export default App;
