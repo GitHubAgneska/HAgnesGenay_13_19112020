2c5d569dfb4da6f58b685b6724e28216ecf41b30
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2022-01-21 19:15:53 +0100

    gitignore comment environment file

---
+++ client/src/utils/environment-dev.jsx
@@ -0,0 +1,8 @@
+
+export const devEnvironment = {
+    bearer: 'temp',
+    apiBaseUrl: 'http://localhost:3001/api/v1',
+    loginEndpoint: '/user/login',
+    signUpEndpoint: '/user/signup',
+    userProfileEndpoint: '/user/profile'
+}
