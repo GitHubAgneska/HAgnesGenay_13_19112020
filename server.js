8c5cd6ad1114b3800a43c17be11c76281288b2f8
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2022-01-29 07:34:14 +0100

    root package.json > fix scripts to run db+express+client concurrently

--- server.js
+++ server.js
@@ -25,13 +25,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
-
 express.static(path.join(__dirname, '/client/build/'))
 // Handle custom routes
 app.use('/api/v1/user', require('./routes/userRoutes'))
