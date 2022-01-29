32e3c12a2a96cf6c46743de4ecdf67970098b0cd
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2022-01-29 20:08:52 +0100

    env vars

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const dbUrl = 'mongodb://127.0.0.1:27017/argentBankDB'
-const databaseUrl = process.env.DATABASE_URL || dbUrl
+const databaseUrl = process.env.DATABASE_URL || dbUrl  
 
 module.exports = async () => {
   try {
